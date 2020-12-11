a99eaddbdd8981447ca0fa7f24d8a9a86bae761a
dagp-ucol <dgentner@ucol.mx>
2020-12-11 02:35:50 -0600

    SumatoriaSerieDos added

--- src/main.js
+++ src/main.js
@@ -1,39 +1,5 @@
 export default class App {
 
-    sumatoriaSerieUnoBETA(numero) {
-        let resultado; resultado = 0;
-        let x = 1;
-        for (x; x<=5; x = x + 1) {
-            resultado = resultado + (1/x);
-            if (x==5) {
-                resultado = resultado + (1/numero);  
-            }
-        }
-    return resultado;
-    }
-
-    
-    sumatoriaSerieDosBETA(numero) {
-    let resultado = 0;
-    let x = 1;
-    while (x<=5) {
-    if ((x % 2)!=0) {
-        if (x==1) {
-            resultado = resultado + 1;
-        } else {
-            
-        }   resultado - resultado - (1/x)
-    } else {
-        resultado - resultado + (1/x)
-    }
-        if (x==5) {
-            resultado = resultado + (1/numero);
-        }
-        x++;
-    }
-    resultado = resultado + 0.10
-    return resultado;
-    }
 
     sumatoriaSerieUno(numero) {
         let resultado; resultado = 0;
