1975f86334a6fc46ca1b3f1581c96f234b906ab1
dagp-ucol <dgentner@ucol.mx>
2020-12-11 01:03:18 -0600

    added missing files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero) {
+        let resultado; resultado = 0;
+        let x = 1
+        for (x; x<=5; x = x + 1) {
+            resultado = resultado + (1/x);
+        }
+        resultado = resultado + (numero);
+    return resultado;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+} // No borrar 
 
 let app = new App();
